182364505455935f5764606aa5ffb0021fafa5fa
George Davies <SeivadLR@users.noreply.github.com>
2015-08-04 10:36:58 +0100

    Update map.js
    reformatted for readability
    closes ukodi-training/ODP-Group5#1

--- map/js/map.js
+++ map/js/map.js
@@ -99,7 +99,11 @@ var country = svg.selectAll(".country").data(countries);
     country
       .on("mousemove", function(d,i) {
       	d3.select(this).style("stroke","red").style("stroke-width","1.2px");
-      	$('#info').html("<h2>" + d.name + "</h2>Population: <span class='population'> " + d.value.toString().replace(/\B(?=(\d(3)+(?!\d))/g, "," ) + "</span>");
+      	$('#info').html("<h2>" 
+      	+ d.name 
+      	+ "</h2>Population: <span class='population'> " 
+      	+ d.value.toString().replace(/\B(?=(\d(3)+(?!\d))/g, "," ) 
+      	+ "</span>");
       })
       .on("click", function(d,i) {
       })
